c60e6747c65dad23c2ac41e230758b566de5d970
Arya Riteshkumar Mirani <122677050+aryamirani@users.noreply.github.com>
2023-08-28 16:50:06 +0400

    Whitelist.py

--- Whitelist.py
+++ Whitelist.py
@@ -1,179 +1,144 @@
-from tkinter import getboolean
-from webbrowser import get
-import face_recognition
 import cv2
-import numpy as np
+import face_recognition
 import os
-import sys
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
+# Load known face encodings and names
+known_face_encodings = []
+known_face_names = ['AUTHORIZED NAME']
 
-video_capture = cv2.VideoCapture(0)
+# Load the reference images for each face
+reference_image_1 = face_recognition.load_image_file('PATH TO A PICTURE OF AUTHORIZED FACE')
+reference_face_encoding_1 = face_recognition.face_encodings(reference_image_1)[0]
+known_face_encodings.append(reference_face_encoding_1)
 
+#reference_image_2 = face_recognition.load_image_file('PATH TO A PICTURE OF AUTHORIZED FACE')
+#reference_face_encoding_2 = face_recognition.face_encodings(reference_image_2)[0]
+#known_face_encodings.append(reference_face_encoding_2)
 
-arya_image = face_recognition.load_image_file(r"D:\SDC\FOCUS-main\FOCUS-main\accepted faces\arya.jpg")
-arya_face_encoding = face_recognition.face_encodings(arya_image)[0]
+# Face Detection
+cap = cv2.VideoCapture(0)
 
-known_face_encodings = [
+tolerance = 0.5
 
-    arya_face_encoding
-]
-known_face_names = [
+r = 0
 
-    "Arya Mirani"
-]
-
-
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
+u = 0
 while True:
-    ret, frame = video_capture.read()
+    ret, frame = cap.read()
 
+    if not ret:
+        print("Error: Failed to capture a frame.")
+        break
 
-    if process_this_frame:
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
 
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
 
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=tolerance)
 
-        rgb_small_frame = small_frame[:, :, ::-1]
-
-
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        while len(face_locations) == 0:
-            print("No faces detected")
-            sys.exit()
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-        face_names = []
-        for face_encoding in face_encodings:
-
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
-
-
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-
-            face_names.append(name)
-
-            if name != "Unknown":
-                os.startfile(r"D:\SDC\FOCUS-main\FOCUS-main\Whitelisted Ports.txt")
-
-                process_this_frame = not process_this_frame
-
-
-                for (top, right, bottom, left), name in zip(face_locations, face_names):
-
-                    top *= 4
-                    right *= 4
-                    bottom *= 4
-                    left *= 4
-
-
-                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-
-                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                    font = cv2.FONT_HERSHEY_DUPLEX
-                    cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-
-                cv2.imshow('Video', frame)
-
-
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    quit()
-
-
-                video_capture.release()
-                cv2.destroyAllWindows()
-
-                
-
-
-            if not face_locations:
-                sys.exit()
-
-            if name == "Unknown":
-                cap = cv2.VideoCapture(0)
-                ret, frame = cap.read()
-                cap.release()
-                cv2.imwrite("D:\SDC\FOCUS-main\FOCUS-main\illegal logins\photo.jpg", frame)
-
-
-
-                #SENDING AN EMAIL FOR UNAUTHORIZED ACCESS
-
-
-                smtp_port = 587                 
-                smtp_server = "smtp.gmail.com"  
-
-                email_from = "aryamirani06@gmail.com"
-                email_list = ["aryaamiranii@gmail.com", "28278.arya@iswkoman.com"]
-
-                pswd = "hgupxhpjnwczunkh" 
-
-                subject = "UNAUTHORIZED LOGIN ATTEMPT !!"
-
-                def send_emails(email_list):
-
-                    for person in email_list:
-
-      
-                        body = f"""
-                        THIS PERSON TRIED TO ACCESS THE WHITELIST WITHOUT AUTHORIZATION """
-
+        name = "Unknown"
         
-                        msg = MIMEMultipart()
-                        msg['From'] = email_from
-                        msg['To'] = person
-                        msg['Subject'] = subject
 
-                        msg.attach(MIMEText(body, 'plain'))
-       
-                        filename = "D:\SDC\FOCUS-main\FOCUS-main\illegal logins\photo.jpg"
-       
-                        attachment= open(filename, 'rb')  # r for read and b for binary
-       
-                        attachment_package = MIMEBase('application', 'octet-stream')
+        if True in matches:
 
-                        attachment_package.set_payload((attachment).read())
-                        
-                        encoders.encode_base64(attachment_package)
-                        
-                        attachment_package.add_header('Content-Disposition', "attachment; filename= " + filename)
-                        
-                        msg.attach(attachment_package)
-       
-                        text = msg.as_string()       
-                        
-                        TIE_server = smtplib.SMTP(smtp_server, smtp_port)
-                        TIE_server.starttls()
-                        TIE_server.login(email_from, pswd)
-                        
-                        print()
-                       
-                        TIE_server.sendmail(email_from, person, text)
-                        
-                        print()
-   
-                    TIE_server.quit()
+            first_match_index = matches.index(True)
 
-                send_emails(email_list)
+            name = known_face_names[first_match_index]
 
-                quit()
-quit()
+        if name == 'AUTHORIZED NAME':
+
+            r+=1
+
+            if r==10:
+
+                print('Access Granted')
+
+                break
+        elif name == 'Unknown':
+
+            u += 1
+            if u == 10:
+
+                print('Unknown')
+                break
+            
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
 
+    cv2.imshow('Face Recognition', frame)
 
+    if u == 5:
 
+        cv2.destroyAllWindows()
+        cv2.imwrite('PATH TO SAVE A PICTURE OF THE UNAUTHORIZED PERSON', frame)
+
+        smtp_port = 587
+        smtp_server = "smtp.gmail.com"
+
+        email_from = "EMAIL FROM WHICH A MAIL IS SENT"
+        email_list = ['LIST OF MAILS TO WHICH A MAIL IS SENT']
+
+        pswd = "APP PASSWORD"
+        subject = "UNAUTHORIZED LOGIN ATTEMPT !!"
+
+        def send_emails(email_list):
+            for person in email_list:
+
+                body = f"""
+                THIS PERSON TRIED TO ACCESS THE WHITELIST WITHOUT AUTHORIZATION """
+                msg = MIMEMultipart()
+
+                msg['From'] = email_from
+
+                msg['To'] = person
+
+                msg['Subject'] = subject
+                msg.attach(MIMEText(body, 'plain'))
+
+                filename = "PATH TO THE SAM EPICTURE OF THE UNAUTHORIZED PERSON"
+
+                attachment = open(filename, 'rb')
+                attachment_package = MIMEBase('application', 'octet-stream')
+
+                attachment_package.set_payload((attachment).read())
+
+                encoders.encode_base64(attachment_package)
+                attachment_package.add_header('Content-Disposition', "attachment; filename= " + filename)
+
+                msg.attach(attachment_package)
+
+                text = msg.as_string()
+                TIE_server = smtplib.SMTP(smtp_server, smtp_port)
                 
+                TIE_server.starttls()
+                TIE_server.login(email_from, pswd)
+
+
+                print()
+                TIE_server.sendmail(email_from, person, text)
+                print()
+
+            TIE_server.quit()
+
+        send_emails(email_list)
+        break
+
+    if r == 5:
+        print('Authorized')
+        
+
+        os.startfile(r"Whitelisted Ports.txt")
+        break
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
